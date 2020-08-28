e5380331383bfb08a101349337592ea0a689da11
hiagopsilva <hiagohps360@gmail.com>
2020-08-27 22:46:38 -0300

    add LoadingTrendingPanel component

--- src/components/RightColumn/index.tsx
+++ src/components/RightColumn/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import LoadingtrendingPanel from '../Shimmer/LoadingtrendingPanel';
 import TrendingPanel from './TrendingPanel';
 
 import { Container } from './styles';
 
-const RightColumn: React.FC = () => {
+const RightColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="right-column">
-      <TrendingPanel />
-      <TrendingPanel />
+      {isLoading ? (
+        <LoadingtrendingPanel />
+      ) : (
+        <> 
+          <TrendingPanel />
+          <TrendingPanel />
+        </>
+      )}
     </Container>
   );
 };
