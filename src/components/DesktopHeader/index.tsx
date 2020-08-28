1733c7c13cff9669b463f5d0840089a89c2fb3ed
hiagopsilva <hiagohps360@gmail.com>
2020-08-27 22:41:45 -0300

    add DesktopHeader

---
+++ src/components/DesktopHeader/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { Container, Wrapper, LinkedinIcon, SearchInput, HomeIcon, NotificationsIcon, ProfileCircle, CaretDownIcon } from './styles';
+
+const DesktopHeader: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <div className="left">
+          <LinkedinIcon />
+          <SearchInput placeholder="Pesquisar" />
+        </div>
+
+        <div className="right">
+          <nav>
+            <button className="active">
+              <HomeIcon />
+              <span>Início</span>
+            </button>
+            <button>
+              <NotificationsIcon />
+              <span>Notificações</span>
+            </button>
+
+            <button>
+              <ProfileCircle src="https://github.com/hiagopsilva.png" />
+              <span>
+                Eu <CaretDownIcon />
+              </span>
+            </button>
+          </nav>
+        </div>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default DesktopHeader;
